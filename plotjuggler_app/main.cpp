ce07e1b5f53c059da599206ca9897f97da25edbf
Davide Faconti <davide.faconti@gmail.com>
2022-07-25 12:38:50 +0200

    save ColorMaps in layout

--- plotjuggler_app/main.cpp
+++ plotjuggler_app/main.cpp
@@ -348,11 +348,11 @@ int main(int argc, char* argv[])
 
   QNetworkRequest request;
 
-  //  QString uuid = settings.value("UUID", QUuid::createUuid().toString()).toString();
-  //  settings.setValue("UUID", uuid);
+  QString uuid = settings.value("UUID", QUuid::createUuid().toString()).toString();
+  settings.setValue("UUID", uuid);
 
-  //  request.setUrl(QUrl(QString("https://l4g9l4.deta.dev/check_updates/%1").arg(uuid))
-  //  ); manager.get(request);
+  request.setUrl(QUrl(QString("https://l4g9l4.deta.dev/check_updates/%1").arg(uuid)));
+  manager.get(request);
 
   MainWindow* w = nullptr;
 
