3af6863a45e3a4d24f8b4e30410f341959950d74
Jonathan Bendes <jonathan@skyspecs.com>
2023-06-02 20:26:04 -0400

    Looking for zcm in alternate directory first

--- plotjuggler_plugins/PluginsZcm/cmake/FindZcm.cmake
+++ plotjuggler_plugins/PluginsZcm/cmake/FindZcm.cmake
@@ -5,8 +5,11 @@
 # Zcm_LIBRARIES - The libraries needed to use Zcm
 # Zcm_DEFINITIONS - Compiler switches required for using Zcm
 
+find_path ( Zcm_INCLUDE_DIR zcm/zcm-cpp.hpp PATHS $ENV{PJ_ZCM_INSTALL_DIR}/include )
 find_path ( Zcm_INCLUDE_DIR zcm/zcm-cpp.hpp )
+find_library ( Zcm_LIBRARY NAMES zcm PATHS $ENV{PJ_ZCM_INSTALL_DIR}/lib )
 find_library ( Zcm_LIBRARY NAMES zcm )
+find_library ( ZcmToolsUtil_LIBRARY NAMES zcm_tools_util PATHS $ENV{PJ_ZCM_INSTALL_DIR}/lib )
 find_library ( ZcmToolsUtil_LIBRARY NAMES zcm_tools_util )
 
 set ( Zcm_LIBRARIES ${Zcm_LIBRARY} ${ZcmToolsUtil_LIBRARY} )
